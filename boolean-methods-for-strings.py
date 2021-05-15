f0c94c8bbb007990e8aa7092969737404ea80dc6
Simran2907 <84228353+Simran2907@users.noreply.github.com>
2021-05-15 16:18:37 +0530

    Add files via upload

---
+++ boolean-methods-for-strings.py
@@ -0,0 +1,23 @@
+print("hello".islower())
+print("Hello".islower())
+print("hello 12#$$%".islower())
+
+print("HELLO".isupper())
+print("HELLo".isupper())
+print("HELLo 121#$#".isupper())
+
+print("Simran Kaur".istitle())
+print("Simran Kaur 1@".istitle())
+
+print("Simran kaur".isalpha())
+print("Simrankaur".isalpha())
+
+print("123456".isnumeric())
+print("1234#$".isnumeric())
+
+print(" ".isspace())
+print("   ".isspace())
+print("  3  ".isspace())
+
+print("Area12".isalnum())
+print("Area 12".isalnum())
