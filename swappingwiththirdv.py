fbaa3a2fcebbb4e5661a86e0ae54f7ad3c5f65cf
Simran Ghotra <84228353+Simran2907@users.noreply.github.com>
2021-06-06 11:02:32 +0530

    Add files via upload

---
+++ swappingwiththirdv.py
@@ -0,0 +1,9 @@
+a = int(input("Enter value of a:"))
+b = int(input("Enter value of b:"))
+print("Before swapping value of a is:",a)
+print("Before swapping value of b is:",b)
+temp = a
+a =b
+b = temp
+print("After swapping value of a is :",a)
+print("After swapping value of b is:",b)
